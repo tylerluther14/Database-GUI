6bdd67a16301c89616591efd7e91e0549105cf6a
racheljohnston <rj0851@ship.edu>
2023-04-17 16:59:22 -0400

    added button functionality

--- src/DisplayTwo.java
+++ src/DisplayTwo.java
@@ -1,51 +1,43 @@
 import java.awt.GridLayout;
-
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
-public class DisplayTwo 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DisplayTwo implements ActionListener
 {
+	//might have to make instance variables for buttons
+	
+	JButton addPlayerButton;
+	JTextField loginID;
+	JTextField playerEmail;
+	JTextField password;
+	String addThisLine;
+	
+	JButton editPlayerButton;
+	JTextField editLoginID;
+	JTextField editPlayerEmail;
+	JTextField editPassword;
+	String editThisLine;
+
+	JButton deletePlayerButton;
+	JTextField deleteLoginID;
+	String deleteThisID;
+	
 	public DisplayTwo()
 	{
 		JFrame frame = new JFrame();
 		
-	//Add Player Panel
-		Border addPlayerBorder = BorderFactory.createTitledBorder("Add Player");
-		JPanel addPlayerPanel = new JPanel();
-		addPlayerPanel.setLayout(new GridLayout(0, 1));
-		addPlayerPanel.setBorder(addPlayerBorder);
-		//text fields for "Add Player" panel
-		JTextField loginID = new JTextField("Login ID");
-		JTextField playerEmail = new JTextField("Player Email");
-		JTextField password = new JTextField("Password");
-		addPlayerPanel.add(loginID);
-		addPlayerPanel.add(playerEmail);
-		addPlayerPanel.add(password);
-		
-	//Edit Player Panel
-		Border editPlayerBorder = BorderFactory.createTitledBorder("Edit Player");
-		JPanel editPlayerPanel = new JPanel();
-		editPlayerPanel.setLayout(new GridLayout(0, 1));
-		editPlayerPanel.setBorder(editPlayerBorder);
-		//texts fields for "Edit Player" panel
-		JTextField editLoginID = new JTextField("Login ID");
-		JTextField editPlayerEmail = new JTextField("Player Email");
-		JTextField editPassword = new JTextField("Password");
-		editPlayerPanel.add(editLoginID);
-		editPlayerPanel.add(editPlayerEmail);
-		editPlayerPanel.add(editPassword);
-		
-	//Delete Player Panel
-		Border deletePlayerBorder = BorderFactory.createTitledBorder("Delete Player");
-		JPanel deletePlayerPanel = new JPanel();
-		deletePlayerPanel.setLayout(new GridLayout(0, 1));
-		deletePlayerPanel.setBorder(deletePlayerBorder);
-		//text fields for "Delete Player" panel
-		JTextField deleteLoginID = new JTextField("Login ID");
-		deletePlayerPanel.add(deleteLoginID);
+		JPanel addPlayerPanel = createAddPlayerPanel();
+		JPanel editPlayerPanel = createEditPlayerPanel();
+		JPanel deletePlayerPanel = createDeletePlayerPanel();
+		JPanel playerListPanel = createPlayerListPanel();
+		JPanel selectedPlayerPanel = createSelectedPlayerPanel();
 		
 	//Add add, edit, and delete panels to one panel
 		JPanel panel1 = new JPanel();
@@ -53,16 +45,26 @@ public class DisplayTwo
 		panel1.add(addPlayerPanel);
 		panel1.add(editPlayerPanel);
 		panel1.add(deletePlayerPanel);
+	
+	//Combine player list and selected player panels
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new GridLayout(2, 0));
+		panel2.add(playerListPanel);
+		panel2.add(selectedPlayerPanel);
 		
-	//Player List Panel
-		Border playerListBorder = BorderFactory.createTitledBorder("Player List");
-		JPanel playerListPanel = new JPanel();
-		playerListPanel.setLayout(new GridLayout(0, 1));
-		playerListPanel.setBorder(playerListBorder);
-		JTextField temp = new JTextField("TEMP: This should be a table of all of the entries in the table");
-		playerListPanel.add(temp);
+	//adds all panels to the frame
+		frame.setLayout(new GridLayout(0, 2));
+		frame.add(panel1);
+		frame.add(panel2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Display Two");
+		frame.pack();
+		frame.setVisible(true);
 		
-	//Selected Player Panel
+	}
+
+	private JPanel createSelectedPlayerPanel() 
+	{
 		Border selectedPlayerBorder = BorderFactory.createTitledBorder("Selected Player");
 		JPanel selectedPlayerPanel = new JPanel();
 		selectedPlayerPanel.setLayout(new GridLayout(0, 2));
@@ -88,26 +90,147 @@ public class DisplayTwo
 		selectedPlayerPanel.add(subPanel);
 		selectedPlayerPanel.add(subPanel2);
 		
-	//Combine player list and selected player panels
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(new GridLayout(2, 0));
-		panel2.add(playerListPanel);
-		panel2.add(selectedPlayerPanel);
+		return selectedPlayerPanel;
+	}
+
+	private JPanel createPlayerListPanel() 
+	{
+		Border playerListBorder = BorderFactory.createTitledBorder("Player List");
+		JPanel playerListPanel = new JPanel();
+		playerListPanel.setLayout(new GridLayout(0, 1));
+		playerListPanel.setBorder(playerListBorder);
+		JTextField temp = new JTextField("TEMP: This should be a table of all of the entries in the table");
+		playerListPanel.add(temp);
 		
-			
-	//adds all panels to the frame
-		frame.setLayout(new GridLayout(0, 2));
-		frame.add(panel1);
-		frame.add(panel2);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Display Two");
-		frame.pack();
-		frame.setVisible(true);
+		return playerListPanel;
+	}
+
+	private JPanel createDeletePlayerPanel() 
+	{
+		Border deletePlayerBorder = BorderFactory.createTitledBorder("Delete Player");
+		JPanel deletePlayerPanel = new JPanel();
+		deletePlayerPanel.setLayout(new GridLayout(0, 1));
+		deletePlayerPanel.setBorder(deletePlayerBorder);
 		
+		//text fields for "Delete Player" panel
+		deleteLoginID = new JTextField("Login ID");
+		deletePlayerPanel.add(deleteLoginID);
+		//add buttons to send whats in text fields
+		deletePlayerButton = new JButton("Delete");
+		deletePlayerButton.addActionListener(
+			new ActionListener()
+			{
+				String loginIDToBeDeletedTemp;
+				public void actionPerformed(ActionEvent e)
+				{
+					loginIDToBeDeletedTemp = deleteLoginID.getText();
+					System.out.println(loginIDToBeDeletedTemp);
+					deleteThisID = loginIDToBeDeletedTemp;
+					
+					//TODO: make it stall somehow before deleting
+					deleteLoginID.setText("Login ID");
+				}
+			}
+			);
+		deletePlayerPanel.add(deletePlayerButton);
+		
+		return deletePlayerPanel;
+	}
+
+	private JPanel createEditPlayerPanel() 
+	{
+		Border editPlayerBorder = BorderFactory.createTitledBorder("Edit Player");
+		JPanel editPlayerPanel = new JPanel();
+		editPlayerPanel.setLayout(new GridLayout(0, 1));
+		editPlayerPanel.setBorder(editPlayerBorder);
+		
+		//texts fields for "Edit Player" panel
+		editLoginID = new JTextField("Login ID");
+		editPlayerEmail = new JTextField("Player Email");
+		editPassword = new JTextField("Password");
+		editPlayerPanel.add(editLoginID);
+		editPlayerPanel.add(editPlayerEmail);
+		editPlayerPanel.add(editPassword);
+		
+		//add buttons to send whats in text fields
+		editPlayerButton = new JButton("Enter");
+		editPlayerButton.addActionListener(
+			new ActionListener()
+			{
+				String editThisLineTemp;
+				public void actionPerformed(ActionEvent e)
+				{						
+					String login = editLoginID.getText();
+					String email = editPlayerEmail.getText();
+					String password = editPassword.getText();
+					
+					editThisLineTemp = login + "," + email + "," + password;
+					System.out.println(editThisLineTemp);
+					editThisLine = editThisLineTemp;
+					
+					//TODO: make it stall for 5 seconds somehow
+					editLoginID.setText("Login ID");
+					editPlayerEmail.setText("Player Email");
+					editPassword.setText("Password");
+				}
+			}
+			);
+		editPlayerPanel.add(editPlayerButton);
+		
+		return editPlayerPanel;
+	}
+
+	private JPanel createAddPlayerPanel() 
+	{
+		Border addPlayerBorder = BorderFactory.createTitledBorder("Add Player");
+		JPanel addPlayerPanel = new JPanel();
+		addPlayerPanel.setLayout(new GridLayout(0, 1));
+		addPlayerPanel.setBorder(addPlayerBorder);
+		
+		//text fields for "Add Player" panel
+		loginID = new JTextField("Login ID");
+		playerEmail = new JTextField("Player Email");
+		password = new JTextField("Password");
+		addPlayerPanel.add(loginID);
+		addPlayerPanel.add(playerEmail);
+		addPlayerPanel.add(password);
+		
+		//add buttons to send whats in text fields
+		addPlayerButton = new JButton("Enter");
+		addPlayerButton.addActionListener(
+			new ActionListener()
+			{
+				String addThisLineTemp;
+				public void actionPerformed(ActionEvent e)
+				{
+					String login = loginID.getText();
+					String email = playerEmail.getText();
+					String tempPassword = password.getText();
+					
+					addThisLineTemp = login + "," + email + "," + tempPassword;
+					System.out.println(addThisLineTemp);
+					addThisLine = addThisLineTemp;
+					
+					//TODO: make it stay for 5 seconds somehow before clearing
+					loginID.setText("Login ID");
+					playerEmail.setText("Player Email");
+					password.setText("Password");
+				}
+			}
+		);
+		addPlayerPanel.add(addPlayerButton);
+		
+		return addPlayerPanel;
 	}
 
 	public static void main (String[] args)
 	{
 		new DisplayTwo();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO Auto-generated method stub	
 	}
 }
