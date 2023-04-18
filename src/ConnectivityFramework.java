824718eadb88c74ea08b5e99e2ef99825ea66a0b
racheljohnston <rj0851@ship.edu>
2023-04-18 18:44:16 -0400

    added CREATE TABLE statements to ConnectivityFramework

--- src/ConnectivityFramework.java
+++ src/ConnectivityFramework.java
@@ -8,6 +8,14 @@ import java.sql.Statement;
  * Contains methods for JDBC and main method that runs the insertion tests
  */
 public class ConnectivityFramework {
+	
+	public static void main(String[] args)
+	{
+		System.out.println(CREATE_CHARACTER_TABLE_STMT);
+		System.out.println(CREATE_PLAYER_TABLE_STMT);
+		System.out.println(CREATE_WEAPON_TABLE_STMT);
+		System.out.println(CREATE_LOCATION_TABLE_STMT);
+	}
  
     /**
      * Activate the JDBC drivers, but is only setup to work with one specific 
@@ -46,17 +54,51 @@ public class ConnectivityFramework {
      */
     public static final String PASSWORD = "Password_12";
     
-    /**
-     * Names for all of the tables we'll be using for each of the displays
-     */
+
     //display one: tyler
-    public static final String CHARACTER_TABLE_NAME = "Character";
+    public static final String CREATE_CHARACTER_TABLE_STMT = 
+    		"CREATE TABLE CharInfo (Char_Name VARCHAR(100) PRIMARY KEY,"
+    		+ "char_Strength REAL NOT NULL CHECK (char_Strength >= 0 AND char_Strength <= 100),"
+    		+ "char_Stamina REAL NOT NULL CHECK (char_Stamina >= 0 AND char_Stamina <= 100),"
+    		+ "char_Current_Hit_Points INTEGER NOT NULL,"
+    		+ "Char_Max_Hit_Points INTEGER NOT NULL,"
+    		+ "Player_ID INTEGER NOT NULL,"
+    		+ "Location_ID INTEGER NOT NULL,"
+    		+ "CONSTRAINT fk_CharInfo_Player"
+    		+ "FOREIGN KEY (Player_ID) REFERENCES Player(P_Login),"
+    		+ "CONSTRAINT fk_CharInfo_Location FOREIGN KEY (Location_ID) REFERENCES Location(LOC_ID)"
+    		+ ");";
+    		
     //display two: rachel
-    public static final String PLAYER_TABLE_NAME = "Player";
+    public static final String CREATE_PLAYER_TABLE_STMT =
+    		"CREATE TABLE Player "
+    		+ "(P_Login INTEGER PRIMARY KEY, "
+    		+ "P_Password VARCHAR(10) NOT NULL CHECK (LENGTH(P_Password) >= 5), "
+    		+ "P_email VARCHAR(50) NOT NULL CHECK (LENGTH(P_email) >= 15));";
+    
     //display three: brian
-    public static final String WEAPON_TABLE_NAME = "Weapon";
+    public static final String CREATE_WEAPON_TABLE_STMT = 
+    		"CREATE TABLE Weapon("
+    		+ "Ability_id INTEGER NOT NULL,"
+    		+ "W_id INTEGER NOT NULL,"
+    		+ "WeaponType VARCHAR(50) NOT NULL,"
+    		+ "Stored VARCHAR(3) NOT NULL,"
+    		+ "W_Name VARCHAR(100) NOT NULL,"
+    		+ "CONSTRAINT pk_weapon PRIMARY KEY (W_id, W_Name),"
+    		+ "CONSTRAINT fk_weapon_ability FOREIGN KEY (Ability_id) REFERENCES Ability(Ability_id),"
+    		+ "CONSTRAINT fk_weapon_id FOREIGN KEY (W_id) REFERENCES  Items(item_ID)"
+    		+ " );";
+    
     //display four: alecia
-    public static final String LOCATION_TABLE_NAME = "Location";
+    public static final String CREATE_LOCATION_TABLE_STMT = 
+    		"CREATE TABLE Location ("
+    		+ "LOC_ID INTEGER PRIMARY KEY,"
+    		+ "Loc_Name VARCHAR(100) NOT NULL CHECK (LENGTH(Loc_Name) >= 3),"
+    		+ "Loc_Size REAL NOT NULL,"
+    		+ "Loc_Type VARCHAR(20) NOT NULL CHECK (LENGTH(Loc_Type) >= 4),"
+    		+ "Lead_Id INTEGER,"
+    		+ "CONSTRAINT fk_leadid FOREIGN KEY (Lead_Id) REFERENCES Location(LOC_ID)"
+    		+ ");";
     
     
     protected Connection m_dbConn = null;
@@ -75,6 +117,13 @@ public class ConnectivityFramework {
       {
         e.printStackTrace();
       }
+    }
+    
+    public String createTables()
+    {
+    	
+    	
+    	return null;
     }
    
    /**
